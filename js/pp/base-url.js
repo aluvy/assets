11d31d2c9474f9aa5cb2022ae0f7852679d1ccd7
miok <aluvy_@naver.com>
2025-12-05 20:09:08 +0900

    base url 테스트#4

---
+++ js/pp/base-url.js
@@ -0,0 +1,12 @@
+(function () {
+  const baseTag = document.getElementById('dynamic-base');
+
+  var hostname = location.hostname;
+  var isLocal = hostname === 'localhost' || hostname === '127.0.0.1' || hostname === '::1';
+
+  var baseHref = isLocal
+    ? '/' // 로컬 주소
+    : '/assets/'; // 실제 서버 주소
+
+  baseTag.href = baseHref;
+})();
