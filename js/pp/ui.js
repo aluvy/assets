304b4179e9aff5b4f59320028da71e63deda999d
miok <aluvy_@naver.com>
2025-12-05 19:01:16 +0900

    css, js 추가

---
+++ js/pp/ui.js
@@ -0,0 +1,16 @@
+const setSwiper = () => {
+  var swiper = new Swiper(".mySwiper", {
+    autoHeight: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  setSwiper()
+});
